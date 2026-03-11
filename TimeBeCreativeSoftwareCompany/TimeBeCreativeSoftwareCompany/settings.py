b7e7d3aa5f1e8d09b2466407ab78e8c853fdcefe
VIktoriya Cherevatenko <100782926+TimeBeCreative@users.noreply.github.com>
2026-03-11 16:00:02 +0000

    Add all allowed hosts

--- TimeBeCreativeSoftwareCompany/TimeBeCreativeSoftwareCompany/settings.py
+++ TimeBeCreativeSoftwareCompany/TimeBeCreativeSoftwareCompany/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = "django-insecure-etheo18$z_e@p3f*ijnjw9i+#q94rb1bbakrbn-eqn_e6h*)vw
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ["*"]
 
 
 # Application definition
