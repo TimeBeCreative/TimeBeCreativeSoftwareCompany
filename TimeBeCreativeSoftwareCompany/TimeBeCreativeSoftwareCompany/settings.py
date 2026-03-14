d37e83b2494869cde341d5aa5caa997eb6c407e3
VIktoriya Cherevatenko <100782926+TimeBeCreative@users.noreply.github.com>
2026-03-14 19:53:10 +0000

    fix

--- TimeBeCreativeSoftwareCompany/TimeBeCreativeSoftwareCompany/settings.py
+++ TimeBeCreativeSoftwareCompany/TimeBeCreativeSoftwareCompany/settings.py
@@ -119,7 +119,7 @@ STATIC_URL = "static/"
 STATICFILES_DIRS = [BASE_DIR / "app/static"]
 STATIC_ROOT = BASE_DIR / "staticfiles"
 
-EMAIL_BACKEND = "django.core.mail.backends.console.EmailBackend"
+EMAIL_BACKEND = "django.core.mail.backends.smtp.EmailBackend"
 EMAIL_HOST = 'smtp.gmail.com'
 EMAIL_PORT = 587
 EMAIL_USE_TLS = True
