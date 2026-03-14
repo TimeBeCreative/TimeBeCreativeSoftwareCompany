35e3305bb34e446fc2a7402760163d885ccd38f7
VIktoriya Cherevatenko <100782926+TimeBeCreative@users.noreply.github.com>
2026-03-14 20:08:36 +0000

    Superfix

--- TimeBeCreativeSoftwareCompany/TimeBeCreativeSoftwareCompany/settings.py
+++ TimeBeCreativeSoftwareCompany/TimeBeCreativeSoftwareCompany/settings.py
@@ -124,5 +124,5 @@ EMAIL_HOST = 'smtp.gmail.com'
 EMAIL_PORT = 587
 EMAIL_USE_TLS = True
 EMAIL_HOST_USER = 'cherevatenkoviktoriya@gmail.com'
-EMAIL_HOST_PASSWORD = 'xvyu yfms mwch tind'
+EMAIL_HOST_PASSWORD = 'xvyuyfmsmwchtind'
 
